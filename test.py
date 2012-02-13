48a8b05d9ff864df663845568407564f22159259
Takashi Matsuno <g0n5uk3@gmail.com>
2012-02-13 21:51:58 +0900

    added tests.

--- test.py
+++ test.py
@@ -18,10 +18,36 @@ def test_luxio_noncluster_append():
     assert io.delete('0001') == True
     assert io.get('0001') == None
 
-@raises(ValueError)
+def test_luxio_bulk_load():
+    io = luxio.LuxIO('./test', db_flag=luxio.DB_FILE_CREAT, index_type=luxio.INDEX_NONCLUSTER)
+    io.enable_bulk_loading()
+    assert io.put('0001', '0001', luxio.INSERT_OVERWRITE) == True
+    assert io.get('0001') == u'0001'
+    assert io.delete('0001') == True
+    assert io.get('0001') == None
+    io.disable_bulk_loading()
+    assert io.put('0001', '0001', luxio.INSERT_OVERWRITE) == True
+    assert io.get('0001') == u'0001'
+    assert io.delete('0001') == True
+    assert io.get('0001') == None
+
+@raises(RuntimeError)
+def test_luxio_read_only():
+    io = luxio.LuxIO('./testxyz', db_flag=luxio.DB_FILE_RDONLY, index_type=luxio.INDEX_NONCLUSTER)
+    io.put('0001', '0001', luxio.INSERT_OVERWRITE)
+
+@raises(RuntimeError)
 def test_luxio_cluster_append():
     io = luxio.LuxIO('./test', db_flag=luxio.DB_FILE_CREAT, index_type=luxio.INDEX_CLUSTER)
-    assert io.put('0001', '0001', luxio.INSERT_APPEND) == True
+    io.put('0001', '0001', luxio.INSERT_APPEND) == True
+
+@nottest
+def test_luxio_close():
+    io = luxio.LuxIO('./test', db_flag=luxio.DB_FILE_CREAT, index_type=luxio.INDEX_NONCLUSTER)
+    assert io.put('0001', '0001', luxio.INSERT_OVERWRITE) == True
+    assert io.close() == True
+    # SEGV
+    assert io.put('0001', '0001', luxio.INSERT_OVERWRITE) == False
 
 @nottest
 def test_luxio_many_insert():
