b6b983b877a8b2b523bb9ab8c809586c33b39cd9
Takashi Matsuno <g0n5uk3@gmail.com>
2012-03-03 22:07:23 +0900

    added append test.

--- test/test.py
+++ test/test.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python
 import luxio
 from nose.tools import *
@@ -5,7 +6,7 @@ from nose.tools import *
 def test_luxio_noncluster_simple():
     io = luxio.LuxIO('./test', db_flag=luxio.DB_FILE_CREAT, index_type=luxio.INDEX_NONCLUSTER)
     assert io.put('0001', '0001', luxio.INSERT_OVERWRITE) == True
-    assert io.get('0001') == u'0001'
+    assert io.get('0001') == '0001'
     assert io.delete('0001') == True
     assert io.get('0001') == None
 
@@ -57,3 +58,13 @@ def test_luxio_many_insert():
 
 def many_insert(io, i):
     assert io.put('%08d' % i, '%08d' % i) == True
+
+@nottest
+def test_luxio_noncluster_append():
+    io = luxio.LuxIO('./test', db_flag=luxio.DB_FILE_CREAT, index_type=luxio.INDEX_NONCLUSTER)
+    for i in range(1000000):
+        yield many_append, io, i
+
+def many_append(io, i):
+    assert io.put('0001', '0001', luxio.INSERT_APPEND) == True
+
